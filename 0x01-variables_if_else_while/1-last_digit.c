ceada1af708b59010cad82d5eafbc44d80930c34
King2H <neg2htt@gmail.com>
2022-07-01 08:13:40 +0000

    Determine whether a number is >5, =0 or <6 not 0, based on its last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point.
- * Determine if the last digit of a random number is greater than than 5, equal to 0 or is less than 6.
+ * main - Entry point
+ * Description: Determine wheather a random number is > 5, = 0 or < 6.
  * Return: (0) on success
  */
 int main(void)
@@ -12,9 +12,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	/* your code goes there */
 	printf("Last digit of %d is %d and is ", n, n % 10);
-
 	if (n % 10 > 5)
 	{
 		printf("greater than 5\n");
@@ -28,5 +27,4 @@ int main(void)
 		printf("less than 6 and not 0\n");
 	}
 	return (0);
-
 }
