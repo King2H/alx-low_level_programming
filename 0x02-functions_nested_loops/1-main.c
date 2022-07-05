cdd45eecb661c6b286a29d519f02088815e3b0e5
King2H <neg2htt@gmail.com>
2022-07-05 14:45:53 +0000

    main for all lowercase alphabet printing added

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Function call for print_alphabet().
+ * Return: (0) for success
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
