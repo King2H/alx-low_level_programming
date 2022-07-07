14c8b0c96d1b18ddbc8e28a8f8ae346f0a3e1575
King2H <neg2htt@gmail.com>
2022-07-07 13:33:54 +0000

    Print single digit numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ *
+ * Description: Prints single digit numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
